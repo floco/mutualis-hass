ed90f00a39bcc070ba20e134a609f528f002fa9f
floco <info@mutualis.com>
2020-05-09 18:14:46 +0200

    many changes

--- packages/avanza_stock/new_avanza_stock_sensors.py
+++ packages/avanza_stock/new_avanza_stock_sensors.py
@@ -1,25 +1,47 @@
 """Create new Avanza Stock Sensors."""
 
 import click
+import csv
 from string import Template
+import jinja2
 
 TEMPLATE_FILE = "avanza_stock.yaml.template"
+TEMPLATE_PAGE_FILE = "avanza_stock.page.yaml.template"
+OUTPUT_FILE_PATH = "../../dwains-theme/addons/more_page/finance/page.yaml"
 
 @click.command()
-@click.argument('name', type=click.STRING)
-@click.argument('id', type=click.INT)
-def cli(name, id):
+# @click.argument('name', type=click.STRING)
+# @click.argument('id', type=click.INT)
+# def cli(name, id):
+def cli():
     """Create new Avanza Stock Sensors.
 
     NAME: The name of the stock.\n
     ID: The Avanza id of the stock. Obtainable via the URL.
     """
-    filein = open( TEMPLATE_FILE )
-    template = Template( filein.read() )
-    result = template.safe_substitute(name=name,id=id)
-    filename = f"avanza_stock_{name}.yaml"
-    with open(filename, "w") as f:
-        f.write(result)
+
+    stocks = []
+    with open('stocks.csv', newline='') as csvfile:
+        reader = csv.DictReader(filter(lambda row: row[0]!='#', csvfile))
+        for row in reader: 
+
+            filein = open( TEMPLATE_FILE )
+            template = Template( filein.read() )
+            name = row['name']
+            stocks.append(name)
+            result = template.safe_substitute(name=name,id=row['id'])
+            filename = f"avanza_stock_{name}.yaml"
+            with open(filename, "w") as f:
+                f.write(result)
+
+    templateLoader = jinja2.FileSystemLoader(searchpath="./")
+    templateEnv = jinja2.Environment(loader=templateLoader)
+    template = templateEnv.get_template(TEMPLATE_PAGE_FILE)
+    outputText = template.render(stocks=stocks)  # this is where to put args to the template renderer
+
+    with open(OUTPUT_FILE_PATH, "w") as f:
+        f.write(outputText)
+
 
 if __name__ == '__main__':
     cli()
